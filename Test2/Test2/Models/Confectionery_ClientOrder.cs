3549c3e96fd18d53dbcbb06b2f01f090ba9bdcad
Padabed <77849594+Padabed@users.noreply.github.com>
2022-06-09 12:13:23 +0200

    test2

---
+++ Test2/Test2/Models/Confectionery_ClientOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Test2.Models
+{
+	public class Confectionery_ClientOrder
+	{
+		public Confectionery_ClientOrder()
+		{
+		}
+
+		public int IdClientOrder { get; set; }
+		public int IdConfectionery { get; set; }
+		public int Amount { get; set; }
+		public string Comments { get; set; }
+
+
+		public virtual Confectionery IdConfectioneryNav { get; set; }
+		public virtual ClientOrder IdClientOrderNav { get; set; }
+	}
+}
+
